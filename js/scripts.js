6c34a5334255c7454b5d3ad0889fc2b449ef6bb5
semhar <semaria.eyzac@gmal.com>
2018-02-24 15:11:26 -0800

    add more html and js features to result

--- js/scripts.js
+++ js/scripts.js
@@ -8,22 +8,35 @@ $(document).ready(function(){
     var nature = $("input:radio[name=nature]:checked").val();
     var food = $("input:radio[name=food]:checked").val();
     var response;
-    if (trip === "Honeymoon" && weather === "Warm" && nature === "Beach"){
-      var response = $("div#Cairo").show();
-    } else if (trip ==="Bussiness Trip" && weather === "Tropical" && food === "Seafood" ){
-      var response = $("div#Bankok").show();
+
+    if ((trip === "Honeymoon") && (weather === "Warm") && (nature != "Desert") && (food === "Mediterranian")){
+      response = $("#Cairo").show();
+      response = $("#Bankok").hide();
+      response = $("#Lapland").hide();
+    } else if ((trip === "Bussiness Trip") && (weather === "Tropical") && (nature === "Beach") && (food === "Seafood")){
+      response = $("#Bankok").show();
+      response = $("#Cairo").hide();
+      response = $("#Lapland").hide();
+    } else if ((trip === "Entertianment") && (weather === "Cold") && (nature != "Town") && (food === "Scandinavian-Soup")){
+      response = $("#Lapland").show();
+      response = $("#Cairo").hide();
+      response = $("#Bankok").hide();
     }
-      $(#result).text(response);
-      alert("result:" + result);
+    $("#result").show();
+    $(".name").text(enterName);
+
+
+
+
 
 //debagger
-    console.log("enterName :" + enterName)
-    console.log("enterDate:" + enterDate)
-    console.log("trip:" + trip)
-    console.log("weather:" + weather);
-    console.log("nature:" + nature);
-    console.log("food:" + food);
-    console.log($("#Cairo").show());
+    //console.log("enterName :" + enterName)
+    //console.log("enterDate:" + enterDate)
+    //console.log("trip:" + trip)
+    //console.log("weather:" + weather);
+    //console.log("nature:" + nature);
+    //console.log("food:" + food);
+    //console.log($("#Cairo").show());
 
     //var result = add(enterName, enterDate);
     //$("#output").text(result);
